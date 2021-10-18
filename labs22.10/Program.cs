fab2a448ba395faa11a902824be228f80e1b2a6d
BASH <85803671+qdqqd1231@users.noreply.github.com>
2021-10-18 23:02:40 +0300

    Update Program.cs

--- labs22.10/Program.cs
+++ labs22.10/Program.cs
@@ -4,36 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace labs22._10
 {
-    public enum BankAccountType
-    { corrent, saving }
-    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input action : fill correct, fill save, show correct or show save ");
-            string type = Console.ReadLine().ToLower();
             Account save = new Account();
             Account corrent = new Account();
-            while (!type.Equals("exit"))
+            bool flag = true;
+            while (flag)
             {
+                Console.WriteLine("Input action : fill corrent, fill save, show correct or show save,Deposit or withdraw money or exit to exit the program : ");
+                string type = Console.ReadLine().ToLower();
+                if (type.Equals("exit"))
+                {
+                    flag = false;
+                }
                 if (type.Equals("fill corrent"))
                 {
-                    Console.WriteLine("money : ");
-                    decimal money = decimal.Parse(Console.ReadLine());
+                    Console.Write("money : ");
+                    decimal money;
+                    while (!decimal.TryParse(Console.ReadLine(), out money)||money<0)
+                    {
+                        Console.WriteLine("Incorrect input money");
+                    }
                     corrent.FillTheDataOfCorrentType(money);
 
                 }
                 else if (type.Equals("fill save"))
                 {
-                    Console.WriteLine("money : ");
-                    decimal money = decimal.Parse(Console.ReadLine());
+                    Console.Write("money : ");
+                    decimal money;
+                    while (!decimal.TryParse(Console.ReadLine(), out money) || money < 0)
+                    {
+                        Console.WriteLine("Incorrect input money");
+                    }
                     save.FillTheDataOfSavingType(money);
 
                 }
-                if (type.Equals("show corrent"))
+                else if (type.Equals("show corrent"))
                 {
                     corrent.WriteTheDataOfCorrentType();
                 }
@@ -41,8 +52,60 @@ namespace labs22._10
                 {
                     save.WriteTheDataOfSavingType();
                 }
-            }
+                else if(type.Equals("deposit"))
+                {
+                    Console.Write("Choose the type of account : saving or corrent\t\t");
+                    string type0 = Console.ReadLine().ToLower();
+                    if (type0.Equals("saving"))
+                    {
+                        Console.Write("enter volue : ");
+                        decimal money;
+                        while (!decimal.TryParse(Console.ReadLine(), out money) || money < 0)
+                        {
+                            Console.WriteLine("Incorrect volue money");
+                        }
+                        save.DepositMoney(money);
+                    }
+                    else if (type0.Equals("corrent"))
+                    {
+                        Console.Write("enter volue : ");
+                        decimal money;
+                        while (!decimal.TryParse(Console.ReadLine(), out money) || money < 0)
+                        {
+                            Console.WriteLine("Incorrect volue money");
+                        }
+                        corrent.DepositMoney(money);
+                    }
+                   
+                }
+                else if (type.Equals("withdraw"))
+                {
+                    Console.Write("Choose the type of account : saving or corrent\t\t");
+                    string type0 = Console.ReadLine().ToLower();
+                    if (type0.Equals("saving"))
+                    {
+                        Console.Write("enter volue : ");
+                        decimal money;
+                        while (!decimal.TryParse(Console.ReadLine(), out money) || money < 0)
+                        {
+                            Console.WriteLine("Incorrect volue money");
+                        }
+                        save.WithdrawMoney(money);
+                    }
+                    else if (type0.Equals("corrent"))
+                    {
+                        Console.Write("enter volue : ");
+                        decimal money;
+                        while (!decimal.TryParse(Console.ReadLine(), out money) || money < 0)
+                        {
+                            Console.WriteLine("Incorrect volue money");
+                        }
+                        corrent.WithdrawMoney(money);
+                    }
 
+                }
+            }
         }
+
     }
 }
