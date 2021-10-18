a07628a1fe9ecd18c86a0fa4e9c0f6e42ee85ee3
Никита Макурин <makurin.nik@list.ru>
2021-10-18 20:09:08 +0300

    Добавьте файлы проекта.

---
+++ labs22.10/account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labs22._10
+{
+    public class Account
+    {
+        private Guid id;
+        private decimal valueOfMoney;
+        private BankAccountType type;
+
+        
+        public  Account FillTheDataOfSavingType(decimal valueOfMoney)
+        {
+            var acc = new Account();
+            acc.type = BankAccountType.corrent;
+            acc.id = new Guid();
+            acc.valueOfMoney = Decimal.Parse(Console.ReadLine());
+            return acc ;    
+        }
+        public  Account FillTheDataOfCorrentType(decimal valueOfMoney)
+        {
+            var acc = new Account();
+            acc.type = BankAccountType.corrent;
+            acc.id = new Guid();
+            acc.valueOfMoney = Decimal.Parse(Console.ReadLine());
+            return acc;
+        }
+
+        public void WriteTheDataOfCorrentType(Account FillTheDataOfCorrentType)
+        {
+            Console.WriteLine($"Type:{type} \n ID : {id} \n Value of money : {valueOfMoney}");           
+        }
+    }
+
+
+
+
+}
